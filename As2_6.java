5f4e2b0c5a80089e644f0d2598c5b5f7c18c4c34
Khalid Hassan Sk <52673145+khalid0007@users.noreply.github.com>
2020-02-11 18:56:54 +0530

    Create As2_6.java

---
+++ As2_6.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class As2_6{
+
+    public static void main(String args[]){
+
+        Integer a = 100;
+
+        System.out.println("a = " + a);
+        int b = a;
+
+        // System.out.println()
+
+        String c = Integer.toString(128547);
+
+        System.out.println("c = " + c);
+
+        Integer d = Integer.parseInt("182491745");
+
+        String e = d.toString();
+
+    }
+}
